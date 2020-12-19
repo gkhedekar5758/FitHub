ef5babc3b65ab70c9606fbcfb4e83ff834a88631
Gaurang Khedekar <khedekar5758@gmail.com>
2020-12-19 15:20:41 +0530

    Home Component Added- WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Common/navbar/navbar.component';
+import { FooterComponent } from './Common/footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './webComponent/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
